5faa818644dc863a72595cdc431a1d0a60f9e6c2
Lucky <aayushyadav373@gmail.com>
2025-03-11 16:31:26 +0530

    testing pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,10 +7,10 @@ export default async function Home() {
   return (
     <div>
       <h1>name:</h1>
-      {user?.username}
+      {user?.username||"lucky"}
       <br />
       <h1>Password</h1>
-      {user?.password}
+      {user?.password||"lucky 123"}
     </div>
   );
 }
